d1aaa114412ba59c7c042ad621405b51be55fce5
Todd York <tjysunset@outlook.com>
2024-12-08 12:13:02 +0800

    Use single graphicsLayer for AnimatedForwardBackwardTransition

--- app/src/main/java/org/sunsetware/phocid/ui/components/AnimatedForwardBackwardTransition.kt
+++ app/src/main/java/org/sunsetware/phocid/ui/components/AnimatedForwardBackwardTransition.kt
@@ -8,10 +8,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.graphics.graphicsLayer
 import kotlin.math.absoluteValue
-import kotlin.math.roundToInt
 import org.sunsetware.phocid.ui.theme.emphasizedEnter
 import org.sunsetware.phocid.ui.theme.emphasizedExit
 
@@ -20,6 +18,11 @@ const val FORWARD_BACKWARD_SLIDE_FRACTION = 0.5f
 @Stable
 fun forwardBackwardTransitionAlpha(position: Float, index: Int): Float {
     return (1 - (position - index).absoluteValue * 2).coerceAtLeast(0f)
+}
+
+@Stable
+fun forwardBackwardTransitionTranslation(position: Float, index: Int, width: Float): Float {
+    return ((index - position).coerceIn(-1f, 1f) * width * FORWARD_BACKWARD_SLIDE_FRACTION)
 }
 
 @Composable
@@ -51,39 +54,47 @@ inline fun <T> AnimatedForwardBackwardTransition(
         }
     }
 
-    Layout(
-        modifier = modifier,
-        content = {
-            val rootAlpha = forwardBackwardTransitionAlpha(position.value, 0)
-            if (keepRoot || rootAlpha > 0) {
-                Box(modifier = Modifier.alpha(rootAlpha)) { content(null) }
-            } else {
-                Box {}
+    Box(modifier = modifier) {
+        val rootAlpha = forwardBackwardTransitionAlpha(position.value, 0)
+        if (keepRoot || rootAlpha > 0) {
+            Box(
+                modifier =
+                    Modifier.graphicsLayer {
+                        alpha = rootAlpha
+                        if (slide) {
+                            translationX =
+                                forwardBackwardTransitionTranslation(position.value, 0, size.width)
+                        }
+                    }
+            ) {
+                content(null)
             }
+        } else {
+            Box {}
+        }
 
-            largerStack.forEachIndexed { index, item ->
-                val alpha = forwardBackwardTransitionAlpha(position.value, index + 1)
+        largerStack.forEachIndexed { index, item ->
+            val alpha = forwardBackwardTransitionAlpha(position.value, index + 1)
+            key(index + 1) {
                 if (alpha > 0) {
-                    Box(modifier = Modifier.alpha(alpha)) { content(item) }
-                } else {
-                    Box {}
-                }
-            }
-        },
-    ) { measurables, constraints ->
-        val placeables = measurables.map { measurable -> measurable.measure(constraints) }
+                    Box(
+                        modifier =
+                            Modifier.graphicsLayer {
+                                this.alpha = alpha
 
-        layout(constraints.maxWidth, constraints.maxHeight) {
-            placeables.forEachIndexed { index, placeable ->
-                placeable.placeRelative(
-                    if (slide)
-                        ((index - position.value).coerceIn(-1f, 1f) *
-                                constraints.maxWidth *
-                                FORWARD_BACKWARD_SLIDE_FRACTION)
-                            .roundToInt()
-                    else 0,
-                    0,
-                )
+                                if (slide) {
+                                    translationX =
+                                        forwardBackwardTransitionTranslation(
+                                            position.value,
+                                            index + 1,
+                                            size.width,
+                                        )
+                                }
+                            }
+                    ) {
+                        content(item)
+                    }
+                }
             }
         }
     }
