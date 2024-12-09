98142facafb817e060c8aee8780f9e2c8fd60402
Todd York <tjysunset@outlook.com>
2024-12-09 15:42:53 +0800

    Bump version to 20241209

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.sunsetware.phocid"
         minSdk = 30
         targetSdk = 35
-        versionCode = 20241208
-        versionName = "20241208"
+        versionCode = 20241209
+        versionName = "20241209"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
