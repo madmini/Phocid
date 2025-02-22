d606c4c3bac86c0faf27681f8709aa5bd2413ed4
Max Jakobitsch <madmini@users.noreply.github.com>
2025-02-22 11:00:24 +0100

    Parse repeating LRC lines (#69)
    * Parse repeating LRC lines
    
    * Replace \d with [0-9]; Replace fastFlatMap with flatMap; Reformat with ktfmt
    
    ---------
    
    Co-authored-by: Todd York <tjysunset@outlook.com>

--- app/src/main/java/org/sunsetware/phocid/data/Lyrics.kt
+++ app/src/main/java/org/sunsetware/phocid/data/Lyrics.kt
@@ -28,9 +28,11 @@ data class Lyrics(val lines: List<Pair<Duration, String>>) {
     }
 }
 
-private val lrcRegex =
+private val lrcLineRegex =
+    Regex("""^(?<timestamps>(?:\[[0-9]+:[0-9]{1,2}\.[0-9]{2,3}])+)(?<text>.*)$""")
+private val lrcTimestampRegex =
     Regex(
-        """^\[(?<minutes>[0-9]+):(?<seconds>[0-9]{1,2})\.((?<milliseconds>[0-9]{3})|(?<centiseconds>[0-9]{2}))](?<text>.*)$"""
+        """\[(?<minutes>[0-9]+):(?<seconds>[0-9]{1,2})\.((?<milliseconds>[0-9]{3})|(?<centiseconds>[0-9]{2}))]"""
     )
 
 @Stable
@@ -42,16 +44,22 @@ fun parseLrc(lrc: ByteArray, charsetName: String?): Lyrics {
 fun parseLrc(lrc: String): Lyrics {
     return Lyrics(
         lrc.lines()
-            .mapNotNull { line ->
-                lrcRegex.matchEntire(line.trimAndNormalize())?.let {
-                    val timestamp =
-                        it.groups["minutes"]!!.value.toInt().minutes +
-                            it.groups["seconds"]!!.value.toInt().seconds +
-                            (it.groups["milliseconds"]?.value?.toInt()?.milliseconds
-                                ?: (it.groups["centiseconds"]!!.value.toInt() * 10).milliseconds)
-                    val text = it.groups["text"]!!.value.trim()
-                    Pair(timestamp, text)
-                }
+            .flatMap { line ->
+                lrcLineRegex.matchEntire(line.trimAndNormalize())?.let { m1 ->
+                    val text = m1.groups["text"]!!.value.trim()
+                    lrcTimestampRegex
+                        .findAll(m1.groups["timestamps"]!!.value)
+                        .map { m2 ->
+                            val timestamp =
+                                m2.groups["minutes"]!!.value.toInt().minutes +
+                                    m2.groups["seconds"]!!.value.toInt().seconds +
+                                    (m2.groups["milliseconds"]?.value?.toInt()?.milliseconds
+                                        ?: (m2.groups["centiseconds"]!!.value.toInt() * 10)
+                                            .milliseconds)
+                            Pair(timestamp, text)
+                        }
+                        .toList()
+                } ?: emptyList()
             }
             .sortedBy { it.first }
     )
